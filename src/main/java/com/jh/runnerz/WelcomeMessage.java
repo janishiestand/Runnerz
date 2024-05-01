fd4f26b81dd01d1ccbcf35866603292e243d688c
janishiestand <janis.hiestand@gmail.com>
2024-05-01 13:26:30 +0200

    Initial push

---
+++ src/main/java/com/jh/runnerz/WelcomeMessage.java
@@ -0,0 +1,11 @@
+package com.jh.runnerz;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class WelcomeMessage {
+
+    public String getWelcomeMessage() {
+        return "Welcome";
+    }
+}
---
+++ src/main/java/com/jh/runnerz/run/Location.java
@@ -0,0 +1,5 @@
+package com.jh.runnerz.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
